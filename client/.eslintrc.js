05b5cf29835a8225ae681560cb7a27e356b27999
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-18 23:03:52 +0200

    chore: add ESLint and Prettier configuration

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}; 
