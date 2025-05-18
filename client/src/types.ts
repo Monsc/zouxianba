e9827fd061f4617b746de095585b1077221c765c
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-18 15:20:52 +0200

    feat: add Post and ConversationListItem interfaces

--- client/src/types.ts
+++ client/src/types.ts
@@ -4,4 +4,32 @@ export interface Message {
   to: string;
   text: string;
   timestamp: string;
+}
+
+export interface Post {
+  id: string;
+  content: string;
+  author: {
+    id: string;
+    username: string;
+    handle: string;
+    avatar: string;
+  };
+  media?: string[];
+  likes: number;
+  comments: number;
+  createdAt: string;
+  updatedAt: string;
+  liked?: boolean;
+}
+
+export interface ConversationListItem {
+  user: {
+    _id: string;
+    id: string;
+    username: string;
+    avatar: string;
+  };
+  content: string;
+  createdAt: string;
 } 
