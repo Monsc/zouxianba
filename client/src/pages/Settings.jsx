8e075acdccf4490181c694351e37b6616b6ebe0a
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-18 23:01:37 +0200

    feat: add Settings page

---
+++ client/src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Settings() {
+  return (
+    <div className="settings-page">
+      <h1>Settings</h1>
+      {/* 你的设置页面内容 */}
+    </div>
+  );
+}
+
+export default Settings; 
