6ac135b3023b0e757d5ae0d5fc886ed8c455cf9b
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-18 04:01:30 +0200

    feat: add ErrorMessage component for displaying error messages

---
+++ client/src/components/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+function ErrorMessage({ message }: ErrorMessageProps) {
+  return (
+    <div className="error-message bg-red-50 dark:bg-red-900/30 border border-red-200 dark:border-red-800 rounded-lg p-4 text-red-600 dark:text-red-400">
+      <p className="text-center">{message}</p>
+    </div>
+  );
+}
+
+export default ErrorMessage; 
