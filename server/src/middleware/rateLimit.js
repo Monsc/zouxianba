1cac0655d1c8c48df26736dedf6319a3e166bcca
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-22 22:45:06 +0200

    chore: remove Redis-related functionality from rateLimit.js

--- server/src/middleware/rateLimit.js
+++ server/src/middleware/rateLimit.js
@@ -1,49 +1,49 @@
 const rateLimit = require('express-rate-limit');
-const Redis = require('ioredis');
-const config = require('../config');
+// const Redis = require('ioredis');
+// const config = require('../config');
 
-// 创建 Redis 客户端
-const redisClient = new Redis({
-  host: config.redis.host,
-  port: config.redis.port,
-  password: config.redis.password,
-  db: config.redis.db
-});
+// // 创建 Redis 客户端
+// const redisClient = new Redis({
+//   host: config.redis.host,
+//   port: config.redis.port,
+//   password: config.redis.password,
+//   db: config.redis.db
+// });
 
-// 监听 Redis 连接事件
-redisClient.on('connect', () => {
-  console.log('Redis client connected');
-});
+// // 监听 Redis 连接事件
+// redisClient.on('connect', () => {
+//   console.log('Redis client connected');
+// });
 
-redisClient.on('error', (err) => {
-  console.error('Redis client error:', err);
-});
+// redisClient.on('error', (err) => {
+//   console.error('Redis client error:', err);
+// });
 
-// 创建自定义 Redis Store
-class CustomRedisStore {
-  constructor(client) {
-    this.client = client;
-  }
+// // 创建自定义 Redis Store
+// class CustomRedisStore {
+//   constructor(client) {
+//     this.client = client;
+//   }
 
-  async increment(key) {
-    const multi = this.client.multi();
-    multi.incr(key);
-    multi.pexpire(key, config.rateLimit.windowMs);
-    const results = await multi.exec();
-    return results[0][1];
-  }
+//   async increment(key) {
+//     const multi = this.client.multi();
+//     multi.incr(key);
+//     multi.pexpire(key, config.rateLimit.windowMs);
+//     const results = await multi.exec();
+//     return results[0][1];
+//   }
 
-  async decrement(key) {
-    await this.client.del(key);
-  }
+//   async decrement(key) {
+//     await this.client.del(key);
+//   }
 
-  async resetKey(key) {
-    await this.client.del(key);
-  }
-}
+//   async resetKey(key) {
+//     await this.client.del(key);
+//   }
+// }
 
 // 创建限流中间件
-const createLimiter = (useRedis = true) => {
+const createLimiter = () => {
   const options = {
     windowMs: config.rateLimit.windowMs,
     max: config.rateLimit.max,
@@ -51,10 +51,6 @@ const createLimiter = (useRedis = true) => {
     legacyHeaders: false,
     message: { error: '请求过于频繁，请稍后再试' }
   };
-
-  if (useRedis) {
-    options.store = new CustomRedisStore(redisClient);
-  }
 
   return rateLimit(options);
 };
