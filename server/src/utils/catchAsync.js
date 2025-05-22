5e7b54b09157efa0098cb4ec4764cb5aa43b7a54
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-22 03:56:11 +0200

    feat: add catchAsync utility function

--- server/src/utils/catchAsync.js
+++ server/src/utils/catchAsync.js
@@ -1,13 +1,12 @@
 /**
  * 异步错误处理包装器
- * 用于统一处理异步函数中的错误
- * @param {Function} fn - 异步函数
- * @returns {Function} 包装后的函数
+ * 用于统一处理异步路由处理函数中的错误
+ * 
+ * @param {Function} fn - 异步路由处理函数
+ * @returns {Function} 包装后的中间件函数
  */
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
+module.exports = function catchAsync(fn) {
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next);
   };
-};
-
-module.exports = { catchAsync }; 
+}; 
