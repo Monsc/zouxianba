446c4be0ca1acd9aea87958ab6cdb4f3ff51ab62
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-22 05:22:53 +0200

    fix: use updateComment function directly in router.put

--- server/src/routes/comments.js
+++ server/src/routes/comments.js
@@ -21,9 +21,7 @@ console.log('updateComment type:', typeof updateComment); // function
 router.post('/', createComment);
 
 // 更新评论
-router.put('/:id', auth, catchAsync(async (req, res) => {
-  throw new AppError('评论功能正在开发中', 501);
-}));
+router.put('/:id', updateComment);
 
 // 删除评论
 router.delete('/:id', auth, catchAsync(async (req, res) => {
