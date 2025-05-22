e6ca79d5eaf1edf17d45d932b432ceff8091a07a
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-22 04:57:13 +0200

    fix: use CommentController.createComment in comments route

--- server/src/routes/comments.js
+++ server/src/routes/comments.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
+const CommentController = require('../controllers/CommentController');
 
 // 获取评论列表
 router.get('/', catchAsync(async (req, res) => {
@@ -14,9 +15,7 @@ router.get('/', catchAsync(async (req, res) => {
 }));
 
 // 创建评论
-router.post('/', auth, catchAsync(async (req, res) => {
-  throw new AppError('评论功能正在开发中', 501);
-}));
+router.post('/', auth, CommentController.createComment);
 
 // 更新评论
 router.put('/:id', auth, catchAsync(async (req, res) => {
