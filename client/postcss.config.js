693a427ffc1e537f8e1a3cffce3be6469798c8e3
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-25 00:46:30 +0200

    fix: 更新 Tailwind CSS PostCSS 插件配置

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
     cssnano: process.env.NODE_ENV === 'production' ? {
       preset: ['default', {
